6a0cf343eb24f1ffa050aaca0ab52609f1479614
Sean Fallmann <jorundr@hotmail.com>
2018-03-08 21:50:01 -0500

    Setup up webpack to use babel

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,5 @@
-console.log('app.js is running')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>Testing 123</p>;
+ReactDOM.render(template, document.getElementById('app'));
---
+++ indecision-app/src/utils.js
@@ -0,0 +1,4 @@
+console.log('utils js is running');
+
+const add = (a, b) => a + b;
+export {add as default};
