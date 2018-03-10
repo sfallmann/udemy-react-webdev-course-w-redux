3c24a0616d501001753e5562371ef17408f45d90
Sean Fallmann <jorundr@hotmail.com>
2018-03-10 09:48:29 -0500

    Configure webpack to utilize scss

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
+    }, {
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ],
+      test: /\.scss$/
     }]
   },
   devtool: 'cheap-module-eval-source-map',
