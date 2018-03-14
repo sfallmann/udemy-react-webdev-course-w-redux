d3695092760ba11aa26001635e8283bbcb74fce4
Sean Fallmann <jorundr@hotmail.com>
2018-03-14 06:51:04 -0400

    Used indecision-app to create boilerplate for new apps and began setup of expensify-app

---
+++ boilerplate/src/app.js
@@ -0,0 +1,8 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>Expensify App</p>, document.getElementById('app'));
