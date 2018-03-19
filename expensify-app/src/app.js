edf4bf63ae53fe5889aa7155d54264d4b4144e26
Sean Fallmann <jorundr@hotmail.com>
2018-03-18 21:46:28 -0400

    Added Header with nav bar using NavLinks

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -31,19 +31,35 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
   <div>
-    404 - not found
+    <p>404 - not found</p>
+    <Link to="/">Go Home</Link>
   </div>
 );
 
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <div>
+      <span><NavLink to="/" activeClassName="is-active" exact={true}> Dashboard </NavLink></span>
+      <span><NavLink to="/create" activeClassName="is-active"> Create Expense </NavLink></span>
+      <span><NavLink to="/edit" activeClassName="is-active"> Edit Expense </NavLink></span>
+      <span><NavLink to="/help" activeClassName="is-active"> Help </NavLink></span>
+    </div>
+  </header>
+)
+
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" exact={true} component={ExpenseDashboardPage}/>
-      <Route path="/create" component={AddExpensePage}/>
-      <Route path="/edit" component={EditExpensePage}/>
-      <Route path="/help" component={HelpPage}/>
-      <Route component={NotFoundPage}/>
-    </Switch>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/" exact={true} component={ExpenseDashboardPage}/>
+        <Route path="/create" component={AddExpensePage}/>
+        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/help" component={HelpPage}/>
+        <Route component={NotFoundPage}/>
+      </Switch>      
+    </div>
   </BrowserRouter>
 );
 
