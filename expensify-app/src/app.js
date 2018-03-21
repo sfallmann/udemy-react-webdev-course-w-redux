87fff9256a476ffc876e8da7ff5607f4ea236732
Sean Fallmann <jorundr@hotmail.com>
2018-03-20 21:07:09 -0400

    Break out expenses and filters reducers and actions into seperate files

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -2,7 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense, editExpense, removeExpense } from './actions/expenses';
+import { setTextFilter, setStartDate, setEndDate, sortByDate, sortByAmount } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+
+store.dispatch(addExpense({description: 'Gas Bill', amount: 6700}));
+store.dispatch(addExpense({description: 'Water Bill', amount: 12000}))
+store.dispatch(setTextFilter('bill'));
+store.dispatch(sortByAmount())
+const state = store.getState();
+console.log(getVisibleExpenses(state.expenses, state.filters))
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
